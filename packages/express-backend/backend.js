c4def1aa77ed28de50b2f7c331da9a0adb4daf55
Taran Singh <tarannssingh@gmail.com>
2024-10-01 10:41:32 -0700

    feat: create endpoint to return user data

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,17 +1,53 @@
 // backend.js
-import express from "express"
+import express from "express";
 
 const app = express();
 const port = 8000;
 
+// This is for the incoming requests
 app.use(express.json()); // apna middleware for express
 
 // Here we are calling a get method and calling the callback function when we get a call for this url pattern
 app.get("/", (req, res) => {
-    res.send("Hanji, Hello World");
-})
+  res.send("Hanji, Hello World");
+});
 
 app.listen(port, () => {
-    console.log(`@ http://localhost:${port}`)
-})
+  console.log(`@ http://localhost:${port}`);
+});
 
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
+// It uses res.json in the background to send the data 
+app.get("/users", (req, res) => {
+    res.send(users)
+})
